12e09fcd740b19c6f23101e7e58d998d9c1f66ed
unknown <pjcs@cin.ufpe.br>
2020-06-04 19:48:28 -0300

    acesso a receita

--- crawler.js
+++ crawler.js
@@ -8,27 +8,67 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-rl.question('O que você deseja cozinhar hoje? ', (answer) => {
-    searching(answer)
 
 
+rl.question('O que você deseja cozinhar hoje? ',  (answer) => {
+    
+    searching(answer,(result)=>{
+         
+        chooseOption(result)
+        
+    })     
+    
+    
 rl.close();
 });
 
-function searching(answer){
+
+function chooseOption(result){
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    for ( var i = 0;i<result.length;i++){console.log(i,': ',result[i].title)}
+
+    rl.question('Escolha uma opção acima ',  (answer) => {
+        acessRecipe(result[answer].link)
+        
+        rl.close()
+    })
+}
+
+
+ function searching(answer, callback){
     request('https://www.tudogostoso.com.br/busca?page=1&q='+answer, function(err,res,body){
 
         if(err) console.log('Erro: ' + err);
-
         var $ = cheerio.load(body);
-        results = []
+        var results = []
 
         $('.col-lg-5 > .rounded').find('.card.recipe-card.recipe-card-with-hover a').each(function(){
             var title = $(this).find('.recipe-title').text().trim()
             var link = $(this).attr('href').trim()
-            results.push([title, link])
+            results.push({title: title, link: link})
         });
-
-        console.log(results)
+        
+        return callback(results)
+        
     });
+}
+function acessRecipe(option, callback){
+    request('https://www.tudogostoso.com.br'+option, function(err,res,body){
+        if(err) console.log('Erro: ' + err);
+        var $ = cheerio.load(body);
+        var results = []
+        console.log($('.col-lg-8.ingredients-card > ul >li>span>p').text().trim())
+        console.log($('.instructions.e-instructions > ol >li>span>p').text().trim())
+        
+        
+        
+
+
+    })
+
 }
